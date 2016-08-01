3d9ab7fedf4dacee9d9ac8724ee9cee4bebcf786
adlerweb <git@adlerweb.info>
2016-08-01 17:56:51 +0200

    Import original

---
+++ fastd-api.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php -f
+<?php
+$url = 'http://register.freifunk-myk.de/srvapi.php';
+$out = '/etc/fastd/vpn/peers/';
+
+
+//Check permissions 
+if(!is_dir($out)) die('Output dir missing');
+if(!is_writable($out)) die('Output dir not writable');
+ 
+//Import new nodes
+$data = file_get_contents($url);
+$data = unserialize($data);
+ 
+foreach($data as $router) {
+	if(!preg_match('/^[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}$/', $router['MAC'])) {
+		trigger_error('Router mit falscher MAC?!', E_USER_WARNING);
+	}elseif(!preg_match('/^[A-F0-9]{64}$/', $router['PublicKey'])) {
+		trigger_error('Router mit falschem Key?!', E_USER_WARNING);
+	}else{
+		$filename='client_'.str_replace(':', '-', $router['MAC']);
+		$fp=fopen($out.$filename, 'w');
+		fwrite($fp, 'key "'.$router['PublicKey'].'";'."\n");
+		fclose($fp);
+		$active[] = $filename;
+	}
+}
+
+//Remove orphan nodes
+$dh = opendir($out);
+while(($file = readdir($dh)) !== false) {
+	if($file != '.' && $file != '..') {
+		if(!in_array($file, $active) && (strpos($file, 'client_') !== false)) {
+			unlink($out.$file);
+		}
+	}
+}
+ 
+//Reload fastd
+exec('killall -SIGHUP fastd');
+ 
+?>
+
